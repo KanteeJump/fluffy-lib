4a7fb9ad5d33b99472dc2ffbabffceffaeede920
KanteeJump <weedjump8@gmail.com>
2024-09-17 06:50:54 -0600

    update ConnectionHandler.php

--- src/handler/ConnectionHandler.php
+++ src/handler/ConnectionHandler.php
@@ -2,6 +2,7 @@
 
 namespace kante\fluffylib\handler;
 
+use Exception;
 use kante\fluffylib\ConnectionStorage;
 use kante\fluffylib\logger\FluffyLogger;
 
@@ -17,7 +18,7 @@ class ConnectionHandler {
     }
 
     public function dropTable(string $table): void {
-        $this->execute("DROP TABLE IF NOT EXISTS $table");
+        $this->execute("DROP TABLE IF EXISTS $table");
         FluffyLogger::success("table $table dropped successfully");
     }
 
@@ -34,21 +35,52 @@ class ConnectionHandler {
 
         $query = "SELECT $columns FROM $table";
 
-        if(!empty($conditions)){
-            $conditions = implode(" AND ", $conditions);
-            $query .= " WHERE $conditions";
-        }
+        $query = $this->where($query, $conditions);
 
         FluffyLogger::success("selecting data from $table successfully");
         return $this->executeFetchAll($query, $values);
+    }
+
+    public function update(string $table, array $set, array $conditions = [], array $values = []): void {
+        $set = count($set) > 1 ? implode("=?, ", $set) : array_shift($set) . "=?";
+        
+        $query = "UPDATE $table SET $set";
+
+        $query =$this->where($query, $conditions);
+
+        $this->execute($query, $values);
+        FluffyLogger::success("data updated successfully");
+    }
+
+    public function delete(string $table, array $conditions = [], array $values = []): void {
+        $query = "DELETE FROM $table";
+
+        $query = $this->where($query, $conditions);
+
+        $this->execute($query, $values);
+        FluffyLogger::success("data deleted successfully");
+    }
+
+    private function where(string $query, array $conditions): string {
+        if(!empty($conditions)){
+            $conditions = array_map(function($value){
+                return "$value?";
+            }, $conditions);
+
+            $conditions = implode(" AND ", $conditions);
+
+            $query .= " WHERE $conditions";
+        }
+
+        return $query;
     }
 
     public function execute(string $query, array $values = []): void {
         try {
             $statement = $this->storage->getConnection()->prepare($query);
             $statement->execute($values);
-        } catch (\Exception $e) {
-            FluffyLogger::error("Error executing query: ".$e->getMessage());
+        } catch (Exception $e) {
+            throw new Exception("Error executing query: ".$e->getMessage());
         }
     }
 
@@ -57,8 +89,8 @@ class ConnectionHandler {
             $statement = $this->storage->getConnection()->prepare($query);
             $statement->execute($values);
             return $statement->fetchAll();
-        } catch (\Exception $e) {
-            FluffyLogger::error("Error executing query: ".$e->getMessage());
+        } catch (Exception $e) {
+            throw new Exception("Error executing query: ".$e->getMessage());
         }
     }
 
